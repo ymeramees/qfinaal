199917dc37a4349cf21850b42c2077926d4d66b4
ymeramees <ymeramees@gmail.com>
2020-10-27 18:14:10 +0200

    Version bumped

--- src/version.h
+++ src/version.h
@@ -1,23 +1,23 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-//Muudetud 28.05.2019 - peale mida oli reliis, seega vaja muuta
-#define VER_FILEVERSION             1,4,15,1
-#define VER_FILEVERSION_STR         "1.4.15\1"
+//Muudetud 27.10.2020 - peale mida oli reliis, seega vaja muuta
+#define VER_FILEVERSION             1,4,16,1
+#define VER_FILEVERSION_STR         "1.4.16\1"
 
-#define VER_PRODUCTVERSION          1,4,15,1
-#define VER_PRODUCTVERSION_STR      "1.4.15\1"
+#define VER_PRODUCTVERSION          1,4,16,1
+#define VER_PRODUCTVERSION_STR      "1.4.16\1"
 
 #define VER_COMPANYNAME_STR         "Ymeramees"
 #define VER_FILEDESCRIPTION_STR     "Finaal 1.4"
 #define VER_INTERNALNAME_STR        "Finaal 1.4"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2019 Ymeramees"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2020 Ymeramees"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "Finaal.exe"
 #define VER_PRODUCTNAME_STR         "Finaal"
 
-#define VER_AASTA                   "2019"
+#define VER_AASTA                   "2020"
 
 //#define VER_COMPANYDOMAIN_STR       "example.org"
 
